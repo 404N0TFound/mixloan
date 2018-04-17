d53ce85bc4d8e4e48207b52226df0eae37d8cf53
liufei <liufei@mia.com>
2018-04-18 00:45:06 +0800

    ganzhehui

--- inc/model/member.php
+++ inc/model/member.php
@@ -265,14 +265,17 @@ class Xuan_mixloan_Member
     }
 
     /**
-    *   获取邀请
-    **/
-    public function getInviter($phone) {
+     *   获取邀请
+     **/
+    public function getInviter($phone, $openid="") {
         global $_W;
-        if (!$phone) {
-            return false;
+        $res = false;
+        if ($phone) {
+            $res = pdo_fetchcolumn("SELECT uid FROM ".tablename("xuan_mixloan_inviter"). " WHERE phone=:phone", array(":phone"=>$phone));
         }
-        $res = pdo_fetchcolumn("SELECT uid FROM ".tablename("xuan_mixloan_inviter"). " WHERE phone={$phone}");
+        if (!$res && $openid) {
+            $res = pdo_fetchcolumn("SELECT `qrcid` FROM ".tablename("qrcode_stat")." WHERE openid=:openid AND uniacid=:uniacid AND type=1 ORDER BY id DESC",array(":openid"=>$openid,":uniacid"=>$_W["uniacid"]));
+        }
         return $res;
     }
 
