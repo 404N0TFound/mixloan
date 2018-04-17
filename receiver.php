a544065bfee638222ada77d17ef9bdb7648a5b5d
liufei <liufei@mia.com>
2018-04-18 00:28:37 +0800

    fix

--- receiver.php
+++ receiver.php
@@ -11,11 +11,39 @@ class Xuan_mixloanModuleReceiver extends WeModuleReceiver {
                 $fans = mc_fetch($uid,array('nickname'));
                 $config = $this->module['config'];
                 if($this->message['scene'] && !empty($fans)){
-                    $scene = pdo_fetchcolumn("SELECT qrcid FROM ".tablename("qrcode_stat")." WHERE openid=:openid AND type=1 ORDER BY id ASC",array(":openid"=>$from));
-                    if ($scene) {
-                        pdo_run("UPDATE ".tablename("qrcode_stat")." SET type=2 WHERE openid='{$from}' AND qrcid<>{$scene}");
+                    //进行粉丝增加通知
+                    $qrcid = pdo_fetchcolumn("SELECT qrcid FROM ".tablename("qrcode_stat")." WHERE openid=:openid AND type=1 ORDER BY id ASC",array(":openid"=>$from));
+                    $my_id = pdo_fetchcolumn("SELECT id FROM ".tablename("xuan_mixloan_member")." WHERE openid=:openid",array(":openid"=>$from));
+                    if ($my_id != $this->message['scene']) {
+                        if ($qrcid) {
+                            pdo_run("UPDATE ".tablename("qrcode_stat")." SET type=2 WHERE openid='{$from}' AND qrcid<>{$qrcid}");
+                        } else {
+                            $qrcid = $this->message['scene'];
+                        }
+                        $openid = pdo_fetchcolumn("SELECT openid FROM ".tablename("xuan_mixloan_member")." WHERE id=:id", array(':id'=>$qrcid));
+                        $wx = WeAccount::create();
+                        $msg = array(
+                            'first' => array(
+                                'value' => "您好，您的好友已通过您的推广二维码关注{$config['title']}",
+                                "color" => "#4a5077"
+                            ),
+                            'keyword1' => array(
+                                'value' => $fans['nickname'],
+                                "color" => "#4a5077"
+                            ),
+                            'keyword2' => array(
+                                'value' => date("Y-m-d H:i:s",time()),
+                                "color" => "#4a5077"
+                            ),
+                            'remark' => array(
+                                'value' => "好友尚未购买代理，莫着急！继续推荐代理，好友购买成功，即可获得{$config['inviter_fee_one']}元奖励",
+                                "color" => "#A4D3EE"
+                            ),
+                        );
+                        $templateId=$config['tpl_notice4'];
+                        $res = $wx->sendTplNotice($openid,$templateId,$msg);
                     } else {
-                        $scene = $this->message['scene'];
+                        pdo_run("UPDATE ".tablename("qrcode_stat")." SET type=2 WHERE openid='{$from}' AND qrcid={$my_id}");
                     }
                 }
             }
