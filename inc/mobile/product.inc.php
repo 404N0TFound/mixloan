609042003a66a795b50cfad3190da2ecb9042bbf
liufei <liufei@mia.com>
2018-04-18 11:22:00 +0800

    fix bug

--- inc/mobile/product.inc.php
+++ inc/mobile/product.inc.php
@@ -140,14 +140,17 @@ if($operation=='index'){
 		if ($openid) {
 			pdo_update('xuan_mixloan_member', array('phone'=>trim($_GPC['phone']), 'certno'=>trim($_GPC['idcard'])), array('id'=>$member['id']));
 		}
-		if (!$inviter_uid) {
-			$insert_i = array(
-				'uniacid' => $_W['uniacid'],
-				'uid' => $inviter,
-				'phone' => trim($_GPC['phone']),
-				'createtime' => time()
-			);
-			pdo_insert('xuan_mixloan_inviter', $insert_i);
+		if (empty($inviter_uid)) {
+			$check = m('member')->checkIfRelation($inviter, $_GPC['phone'], $member['openid']);
+			if ($check == false) {
+				$insert_i = array(
+					'uniacid' => $_W['uniacid'],
+					'uid' => $inviter,
+					'phone' => trim($_GPC['phone']),
+					'createtime' => time()
+				);
+				pdo_insert('xuan_mixloan_inviter', $insert_i);
+			}
 		}
 		$status = 0;
 	} else {
