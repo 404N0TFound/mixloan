bef3c63c0acc0869636427065193dbba62ac6263
liufei <liufei@mia.com>
2018-05-10 00:12:26 +0800

    fix

--- inc/web/agent.inc.php
+++ inc/web/agent.inc.php
@@ -178,7 +178,7 @@ if ($operation == 'list') {
     $member = pdo_fetch('select avatar,nickname from '.tablename("xuan_mixloan_member")." where id=:id",array(':id'=>$item['uid']));
     $bank = pdo_fetch('select realname,bankname,banknum,phone from '.tablename("xuan_mixloan_creditCard")." where id=:id",array(':id'=>$item['bank_id']));
     if ($_GPC['post'] == 1) {
-        if ($_GPC['data']['status'] == 1) {
+        if ($_GPC['data']['status'] == 1 && !empty($item['ext_info']['SN'])) {
             $ACC_NO = $bank['banknum'];
             $ACC_NAME = $bank['realname'];
             $AMOUNT = $item['bonus'];
@@ -187,11 +187,12 @@ if ($operation == 'list') {
             $MER_ORDER_NO = 'ON' . time();
             $BATCH_NO = 'RHB' . date('Ymd') . $SN;
             require_once('../addons/xuan_mixloan/lib/yilian_pay/pay.php');
+            var_dump($res);die;
             if ($res['TRANS_STATE'] != "0000") {
-                message('打款失败', '', 'error');
+                message('打款失败', $this->createWebUrl('agent', array('op' => 'withdraw_list')), 'error');
             }
-            if ($res['PAY_STATE'] != '00A4') {
-                message($res['TRANS_DETAILS'][0]['REMARK'], '', 'error');
+            if ($res['PAY_STATE'] != '0000') {
+                message($res['TRANS_DETAILS'][0]['REMARK'], $this->createWebUrl('agent', array('op' => 'withdraw_list')), 'error');
             }
             $SN = $res['TRANS_DETAILS'][0]['SN'];
             $MER_ORDER_NO = $res['TRANS_DETAILS'][0]['MER_ORDER_NO'];
