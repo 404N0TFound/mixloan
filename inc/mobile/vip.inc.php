70111b92aff72602b8974118ec05a1c0bc4d4c3b
liufei <liufei@mia.com>
2018-05-16 21:26:33 +0800

    提现最低限额调整

--- inc/mobile/vip.inc.php
+++ inc/mobile/vip.inc.php
@@ -185,6 +185,9 @@ if($operation=='buy'){
 	if (!$bonus) {
 		show_json(-1, null, "提现金额不能为0");
 	}
+	if ($bonus<100) {
+		show_json(-1, null, "提现金额不能低于100");
+	}
 	if (!$bank_id) {
 		show_json(-1, null, "请选择提现银行卡");
 	}
