f37e9695b54db8f47f4946263b8426fbd41e02b1
liufei <liufei@mia.com>
2018-05-26 13:45:35 +0800

    fix

--- inc/mobile/product.inc.php
+++ inc/mobile/product.inc.php
@@ -204,7 +204,7 @@ if($operation=='index'){
             $account = WeAccount::create($_W['acid']);
             $account->sendTplNotice($inviter_openid, $config['tpl_notice1'], $datam, $url);
             if ($openid) {
-                pdo_update('xuan_mixloan_member', array('phone'=>trim($_GPC['phone']), 'certno'=>trim($_GPC['idcard'])), array('id'=>$member['id']));
+                // pdo_update('xuan_mixloan_member', array('phone'=>trim($_GPC['phone']), 'certno'=>trim($_GPC['idcard'])), array('id'=>$member['id']));
             }
             if (!$inviter_uid) {
                 $check = m('member')->checkIfRelation($inviter, $member['id']);
