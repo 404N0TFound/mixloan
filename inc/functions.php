945c2b0f342c64a17013fb2931a92b0d94e8a34b
liufei <liufei@mia.com>
2018-05-29 10:16:51 +0800

    fix

--- inc/functions.php
+++ inc/functions.php
@@ -177,7 +177,7 @@ function RGBToHex($rgb){
 *   缩短地址
 **/
 function shortUrl($target) {
-    return target;
+    return $target;
     $target_url = urlencode($target);
     $short = pdo_fetch("SELECT short_url,createtime FROM ".tablename("xuan_mixloan_shorturl")." WHERE target_url=:target_url ORDER BY id DESC", array(':target_url'=>$target));
     if (!$short || $short['createtime'] < time()-86400) {
