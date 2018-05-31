81fcd3d58fd1c11de7d18be6967e08d41924f930
liufei <liufei@mia.com>
2018-05-31 10:36:59 +0800

    fix

--- inc/model/product.php
+++ inc/model/product.php
@@ -273,7 +273,7 @@ class Xuan_mixloan_Product
         $inviter = (int)$params['inviter'];
         if ($params['begin']) {
             $begin = strtotime($params['begin']);
-            $end = strtotime($params['begin']." +1 month -1 day");
+            $end = strtotime($params['begin']." +1 month");
             $wheres .= " AND createtime>={$begin} AND createtime<{$end}";
         }
         if ($type == 1) {
@@ -304,7 +304,7 @@ class Xuan_mixloan_Product
         global $_W;
         $inviter = (int)$params['inviter'];
         $begin = strtotime($params['begin']);
-        $end = strtotime($params['begin']." +1 month -1 day");
+        $end = strtotime($params['begin']." +1 month");
         $fields = "COUNT(1) AS count";
         $sql = "SELECT {$fields} FROM ".tablename("xuan_mixloan_product_apply")." WHERE uniacid={$_W['uniacid']} AND createtime>={$begin} AND createtime<{$end} AND inviter={$inviter} AND pid<>0";
         $res = pdo_fetchcolumn($sql);
