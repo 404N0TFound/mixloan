a97273331ea0981ab51db1214c8ae8c7379a8b6a
liufei <liufei@mia.com>
2018-06-04 11:27:09 +0800

    fix

--- inc/mobile/vip.inc.php
+++ inc/mobile/vip.inc.php
@@ -308,10 +308,14 @@ if($operation=='buy'){
     $temp_time = date('Y-m-d',time() - ((date('w') == 0 ? 7 : date('w')) - 1) * 24 * 3600);
     $start_time = strtotime($temp_time);
     $end_time = strtotime("+1 month {$temp_time}");
-    $list = pdo_fetchall("SELECT inviter,SUM(re_bonus+done_bonus+extra_bonus) AS bonus FROM ".tablename('xuan_mixloan_bonus')." WHERE relate_id=0 AND createtime>{$start_time} AND createtime<{$end_time} GROUP BY inviter HAVING bonus<>0 ORDER BY bonus DESC LIMIT 15");
+    // $list = pdo_fetchall("SELECT inviter,SUM(re_bonus+done_bonus+extra_bonus) AS bonus FROM ".tablename('xuan_mixloan_bonus')." WHERE relate_id=0 AND createtime>{$start_time} AND createtime<{$end_time} GROUP BY inviter HAVING bonus<>0 ORDER BY bonus DESC LIMIT 15");
+    $list = pdo_fetchall("SELECT uid,SUM(bonus) as bonus FROM " .tablename('xuan_mixloan_withdraw'). "
+    	WHERE createtime>{$start_time} AND createtime<{$end_time}
+    	GROUP BY uid HAVING bonus<>0
+    	ORDER BY bonus DESC LIMIT 15");
     if (!empty($list)) {
         foreach ($list as &$row) {
-            $temp_member = pdo_fetch("SELECT nickname,avatar,phone FROM ".tablename('xuan_mixloan_member').' WHERE id=:id', array(':id'=>$row['inviter']));
+            $temp_member = pdo_fetch("SELECT nickname,avatar,phone FROM ".tablename('xuan_mixloan_member').' WHERE id=:id', array(':id'=>$row['uid']));
             $row['nickname'] = $temp_member['nickname'];
             $row['avatar'] = $temp_member['avatar'];
             $row['phone'] = substr($temp_member['phone'], 0, 4) . '****' . substr($temp_member['phone'], -3, 3);
