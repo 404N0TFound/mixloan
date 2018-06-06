625573043fc8bd25fb975cfd4c2f66253487b9b6
liufei <liufei@mia.com>
2018-06-06 10:14:23 +0800

    fix

--- inc/mobile/ajax.inc.php
+++ inc/mobile/ajax.inc.php
@@ -10,7 +10,7 @@ if($operation == 'getCode'){
 	$time = time()-86400;
 	$cache =  rand(111111,999999);
 	$content = "尊敬的用户，您的本次验证码为：{$cache}";
-	if (isset($_COOKIE['cache_code'])) {gi
+	if (isset($_COOKIE['cache_code'])) {
 		show_json(-1, null, "您的手太快啦，请休息会再获取");
 	}
     $res = setcookie('cache_code', md5($phone.$cache), time()+90);
