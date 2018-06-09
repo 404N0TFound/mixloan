04416ca89c3fa87372c7580b07ff5d6458d32587
liufei <liufei@mia.com>
2018-06-09 18:31:22 +0800

    fix

--- inc/mobile/index.inc.php
+++ inc/mobile/index.inc.php
@@ -123,4 +123,55 @@ if($operation=='register'){
     } else {
         show_json(1, ['url'=>$this->createMobileUrl('index', array('op'=>'login'))], "更改密码成功，请牢记您的密码");
     }
+} else if ($operation == 'find_user') {
+    //找回账号
+    if (!$config['backup']) {
+        message('找回账号暂未开放', $this->createMobileUrl('user'), 'error');
+    }
+    include $this->template('index/find_user');
+} else if ($operation == 'find_user_submit') {
+    //找回账号提交
+    $phone = trim($_GPC['phone']);
+    if (!$config['backup']) {
+        show_json(-1, [], '找回账号暂未开放');
+    }
+    if (!empty($member['phone'])) {
+        show_json(-1, [], '您的手机已绑定，无法使用此功能');
+    }
+    $smsCode = $_GPC['smsCode'];
+    if (md5($phone.$smsCode) != $_COOKIE['cache_code']) {
+        show_json(-1, [], "验证码不符或验证码已失效");
+    }
+    $old_man = pdo_fetch('SELECT id,nickname FROM ' .tablename('xuan_mixloan_member'). '
+        WHERE phone=:phone ORDER BY id DESC', array(':phone'=>$phone));
+    if (empty($old_man)) {
+        show_json(-1, [], '该手机号未绑定任何信息');
+    }
+    show_json(1, [], "你要找回的账号昵称为{$old_man['nickname']}");
+} else if ($operation == 'post_find') {
+    //找回账号提交
+    $phone = trim($_GPC['phone']);
+    if (!$config['backup']) {
+        show_json(-1, [], '找回账号暂未开放');
+    }
+    if (!empty($member['phone'])) {
+        show_json(-1, [], '您的手机已绑定，无法使用此功能');
+    }
+    $smsCode = $_GPC['smsCode'];
+    if (md5($phone.$smsCode) != $_COOKIE['cache_code']) {
+        show_json(-1, [], "验证码不符或验证码已失效");
+    }
+    $old_man = pdo_fetch('SELECT id,openid,uniacid,uid FROM ' .tablename('xuan_mixloan_member'). '
+        WHERE phone=:phone ORDER BY id DESC', array(':phone'=>$phone));
+    if (empty($old_man)) {
+        show_json(-1, [], '该手机号未绑定任何信息');
+    }
+    pdo_update('xuan_mixloan_member', array('openid'=>$openid, 'uniacid'=>$_W['uniacid'], 'uid'=>$member['uid']), array('id'=>$old_man['id']));
+    pdo_update('qrcode_stat', array('openid'=>$openid, 'uniacid'=>$_W['uniacid']), array('openid'=>$old_man['openid']));
+    pdo_update('xuan_mixloan_inviter', array('uniacid'=>$_W['uniacid']), array('uid'=>$old_man['id']));
+    pdo_update('xuan_mixloan_friend', array('openid'=>$openid), array('openid'=>$old_man['openid']));
+    pdo_update('xuan_mixloan_post_looks', array('openid'=>$openid), array('openid'=>$old_man['openid']));
+    pdo_update('xuan_mixloan_friend_comment', array('openid'=>$openid), array('openid'=>$old_man['openid']));
+    pdo_update('xuan_mixloan_member', array('openid'=>$old_man['openid'], 'uniacid'=>$old_man['uniacid'], 'uid'=>$old_man['uid']), array('id'=>$member['id']));
+    show_json(1, ['url'=>$this->createMobileUrl('user', ['op'=>''])], "找回账户成功");
 }
