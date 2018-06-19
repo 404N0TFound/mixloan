e227406f4a326b68dc6260d773b496871748e1ab
liufei <liufei@mia.com>
2018-06-19 20:49:49 +0800

    fix

--- inc/mobile/index.inc.php
+++ inc/mobile/index.inc.php
@@ -233,4 +233,15 @@ if($operation=='register'){
     pdo_update('xuan_mixloan_friend_comment', array('openid'=>$openid), array('openid'=>$old_man['openid']));
     pdo_update('xuan_mixloan_member', array('openid'=>$old_man['openid'], 'uniacid'=>$old_man['uniacid'], 'uid'=>$old_man['uid']), array('id'=>$member['id']));
     show_json(1, ['url'=>$this->createMobileUrl('user', ['op'=>''])], "找回账户成功");
+} else if ($operation == 'exit') {
+    die("<!DOCTYPE html>
+    <html>
+        <head>
+            <meta name='viewport' content='width=device-width, initial-scale=1, user-scalable=0'>
+            <title>抱歉，出错了</title><meta charset='utf-8'><meta name='viewport' content='width=device-width, initial-scale=1, user-scalable=0'><link rel='stylesheet' type='text/css' href='https://res.wx.qq.com/connect/zh_CN/htmledition/style/wap_err1a9853.css'>
+        </head>
+        <body>
+        <div class='page_msg'><div class='inner'><span class='msg_icon_wrp'><i class='icon80_smile'></i></span><div class='msg_content'><h4>请在APP客户端打开</h4></div></div></div>
+        </body>
+    </html>");
 }
