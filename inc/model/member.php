fc3216a704594f84c67a6b3cc3d9fab6ecd0cd46
liufei <liufei@mia.com>
2018-06-21 23:08:14 +0800

    fix

--- inc/model/member.php
+++ inc/model/member.php
@@ -57,7 +57,7 @@ class Xuan_mixloan_Member
     {
         global $_W;
         $uid = intval($openid);
-        if (empty($uid)) {
+        if (empty($uid) || strlen($openid) == 32) {
             $info = pdo_fetch('select * from ' . tablename('xuan_mixloan_member') . ' where  openid=:openid and uniacid=:uniacid limit 1', array(
                 ':uniacid' => $_W['uniacid'],
                 ':openid' => $openid
@@ -161,16 +161,17 @@ class Xuan_mixloan_Member
             $openid = m('user')->getOpenid();
         }
         if (empty($openid)) {
-            die("<!DOCTYPE html>
-            <html>
-                <head>
-                    <meta name='viewport' content='width=device-width, initial-scale=1, user-scalable=0'>
-                    <title>抱歉，出错了</title><meta charset='utf-8'><meta name='viewport' content='width=device-width, initial-scale=1, user-scalable=0'><link rel='stylesheet' type='text/css' href='https://res.wx.qq.com/connect/zh_CN/htmledition/style/wap_err1a9853.css'>
-                </head>
-                <body>
-                <div class='page_msg'><div class='inner'><span class='msg_icon_wrp'><i class='icon80_smile'></i></span><div class='msg_content'><h4>请在微信客户端打开链接</h4></div></div></div>
-                </body>
-            </html>");
+//            die("<!DOCTYPE html>
+//            <html>
+//                <head>
+//                    <meta name='viewport' content='width=device-width, initial-scale=1, user-scalable=0'>
+//                    <title>抱歉，出错了</title><meta charset='utf-8'><meta name='viewport' content='width=device-width, initial-scale=1, user-scalable=0'><link rel='stylesheet' type='text/css' href='https://res.wx.qq.com/connect/zh_CN/htmledition/style/wap_err1a9853.css'>
+//                </head>
+//                <body>
+//                <div class='page_msg'><div class='inner'><span class='msg_icon_wrp'><i class='icon80_smile'></i></span><div class='msg_content'><h4>请在微信客户端打开链接</h4></div></div></div>
+//                </body>
+//            </html>");
+            header("location:http://wx.luohengwangluo.com/app/index.php?i=2&c=entry&op=login&do=index&m=xuan_mixloan");
             return;
         }
         $member   = m('member')->getMember($openid);
@@ -290,6 +291,46 @@ class Xuan_mixloan_Member
         }
         $res = pdo_fetch("SELECT phone,openid,nickname FROM ".tablename("xuan_mixloan_member"). " WHERE id={$uid}");
         return $res;
+    }
+    /**
+     *   口子进来的锁定上级
+     *
+     */
+    public function checkFirstInviter($openid, $inviter) {
+        global $_W;
+        $openid = trim($openid);
+        $inviter = intval($inviter);
+        if (empty($openid) || empty($inviter)) {
+            return false;
+        }
+        $id = pdo_fetchcolumn('select id from ' . tablename('xuan_mixloan_member') . '
+            where openid=:openid', array(
+            ':openid' => $openid
+        ));
+        if ($id == $inviter) {
+            return false;
+        }
+        $res = pdo_fetchcolumn("SELECT count(*) FROM " . tablename("qrcode_stat") . "
+            WHERE openid=:openid AND uniacid=:uniacid AND type=1", array(
+            ":openid" => $openid,
+            ":uniacid" => $_W["uniacid"]
+        ));
+        if (!$res) {
+            $insert = array(
+                'uniacid' => $_W['uniacid'],
+                'acid' => 0,
+                'qid' => 0,
+                'openid' => $openid,
+                'type' => 1,
+                'qrcid' => $inviter,
+                'scene_str' => $inviter,
+                'createtime' => time() ,
+            );
+            pdo_insert('qrcode_stat', $insert);
+            return true;
+        } else {
+            return false;
+        }
     }
     /**
      *   检查uid的上下三级是否和inviter存在关系
