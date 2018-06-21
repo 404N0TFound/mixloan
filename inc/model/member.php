73ee0fcafe296740274352997ae1f7a80173cba7
liufei <liufei@mia.com>
2018-06-21 23:31:12 +0800

    fix

--- inc/model/member.php
+++ inc/model/member.php
@@ -292,6 +292,46 @@ class Xuan_mixloan_Member
         return $res;
     }
     /**
+     *   口子进来的锁定上级
+     *
+     */
+    public function checkFirstInviter($openid, $inviter) {
+        global $_W;
+        $openid = trim($openid);
+        $inviter = intval($inviter);
+        if (empty($openid) || empty($inviter)) {
+            return false;
+        }
+        $id = pdo_fetchcolumn('select id from ' . tablename('xuan_mixloan_member') . '
+            where openid=:openid', array(
+            ':openid' => $openid
+        ));
+        if ($id == $inviter) {
+            return false;
+        }
+        $res = pdo_fetchcolumn("SELECT count(*) FROM " . tablename("qrcode_stat") . "
+            WHERE openid=:openid AND uniacid=:uniacid AND type=1", array(
+            ":openid" => $openid,
+            ":uniacid" => $_W["uniacid"]
+        ));
+        if (!$res) {
+            $insert = array(
+                'uniacid' => $_W['uniacid'],
+                'acid' => 0,
+                'qid' => 0,
+                'openid' => $openid,
+                'type' => 1,
+                'qrcid' => $inviter,
+                'scene_str' => $inviter,
+                'createtime' => time() ,
+            );
+            pdo_insert('qrcode_stat', $insert);
+            return true;
+        } else {
+            return false;
+        }
+    }
+    /**
      *   检查uid的上下三级是否和inviter存在关系
      **/
     public function checkIfRelation($inviter, $uid) {
