dbbd5372b8448710d8eadb988cf31fb418eae8ba
liufei <liufei@mia.com>
2018-06-21 14:02:09 +0800

    fix

---
+++ inc/model/applyApi.php
@@ -0,0 +1,39 @@
+<?php
+defined('IN_IA') or exit('Access Denied');
+class Xuan_mixloan_ApplyApi
+{
+    private $host = "https://api.51ley.com/apis";
+    private $station_id = "974483935490736129";
+    private $user_id = "1003477013291335680";
+
+    /*
+     * 获取银行列表
+     */
+    public function bankList()
+    {
+        $url = $this->host . 'api/bank';
+        $params['stationid'] = $this->station_id;
+        $query_url = $url . '?' . $this->GetHttpQueryString($params);
+        $result = file_get_contents($query_url);
+        var_dump($result);die;
+    }
+    /**
+     * 数组转get方式
+     * @param $params
+     * @return string
+     */
+    function GetHttpQueryString($params)
+    {
+        if (!empty($params) && !is_array($params)) {
+            return "";
+        }
+        ksort($params);
+        $str = "";
+        foreach ($params as $k => $v)
+        {
+            $str .= $k ."=". $v ."&";
+        }
+        $str = rtrim($str, "&");
+        return $str;
+    }
+}
