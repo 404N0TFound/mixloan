43b99a4edd13fc405306da32c27ce4cbc11689a2
liufei <liufei@mia.com>
2018-06-26 16:32:00 +0800

    fix

--- inc/mobile/loan.inc.php
+++ inc/mobile/loan.inc.php
@@ -8,7 +8,7 @@ $member = m('member')->getMember($openid);
 $member['user_type'] = m('member')->checkAgent($member['id']);
 if($operation=='index'){
 	//贷款中心首页
-	$list = m('loan')->getList();
+	$list = m('loan')->getList([], [], ' apply_nums desc', 10);
 	$advs = m('loan')->getAdvs();
 	$barrages = m('loan')->getBarrage($list);
 	include $this->template('loan/index');
