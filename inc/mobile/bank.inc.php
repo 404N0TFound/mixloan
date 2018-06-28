0ce92eefc26478786cc25b84ec1b7edffcf159d6
liufei <liufei@mia.com>
2018-06-28 20:05:59 +0800

    fix

--- inc/mobile/bank.inc.php
+++ inc/mobile/bank.inc.php
@@ -8,7 +8,18 @@ $member = m('member')->getMember($openid);
 $member['user_type'] = m('member')->checkAgent($member['id']);
 if ($operation == 'extend_limit') {
 	//提升额度
-	$banks = m('bank')->getList();
+    $bank = array();
+    $temp_list = m('bank')->getList();
+    $count = 0;
+    foreach ($temp_list as $value) {
+        $count++;
+        $temp[] = $value;
+        if ($count==3) {
+            $banks[] = $temp;
+            $temp = array();
+            $count = 0;
+        }
+    }
 	include $this->template('bank/extend_limit');
 } else if ($operation == 'extend_by_phone') {
 	//获取电话提额
@@ -43,7 +54,18 @@ if ($operation == 'extend_limit') {
 	show_json(1, $ret);
 } else if ($operation == 'extend_query') {
 	//办卡查询
-	$banks = m('bank')->getList();
+    $bank = array();
+    $temp_list = m('bank')->getList();
+    $count = 0;
+    foreach ($temp_list as $value) {
+        $count++;
+        $temp[] = $value;
+        if ($count==3) {
+            $banks[] = $temp;
+            $temp = array();
+            $count = 0;
+        }
+    }
 	include $this->template('bank/extend_query');
 } else if ($operation == 'extend_tips') {
 	//提额技巧
@@ -66,7 +88,17 @@ if ($operation == 'extend_limit') {
 	show_json(1, array_values($list));
 } else if ($operation == 'want_subscribe') {
 	//我要办卡
-	$banks = m('bank')->getList();
+    $temp_list = m('bank')->getList();
+    $count = 0;
+    foreach ($temp_list as $value) {
+        $count++;
+        $temp[] = $value;
+        if ($count==4) {
+            $banks[] = $temp;
+            $temp = array();
+            $count = 0;
+        }
+    }
 	$get = ['id', 'name', 'apply_nums', 'ext_info'];
 	$list = m('bank')->getCard($get);
 	$recommends = m('bank')->getRecommendCard($list);
@@ -145,5 +177,19 @@ if ($operation == 'extend_limit') {
 		$share_image = tomedia($config['share_image']);
 	}
 	include $this->template('bank/artical');
-} 
-?>
+}  else if ($operation == 'display') {
+    //银行展示页
+    $id = intval($_GPC['id']);
+    if (empty($id)) {
+        message('出错了', '', 'error');
+    }
+    $item = m('bank')->getCard(['id', 'ext_info'], ['id'=>$id])[$id];
+    if (empty($item)) {
+        message('出错了', '', 'error');
+    }
+    if (is_weixin()) {
+        header("location:{$item['ext_info']['url']}");
+        exit();
+    }
+    include $this->template('bank/display');
+}
