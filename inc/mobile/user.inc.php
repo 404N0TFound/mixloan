4cf6e518fe425ed57ab3d70ec7b1910a33c7ed1b
liufei <liufei@mia.com>
2018-06-29 15:48:23 +0800

    fix

--- inc/mobile/user.inc.php
+++ inc/mobile/user.inc.php
@@ -8,7 +8,7 @@ $member = m('member')->getMember($openid);
 $member['user_type'] = m('member')->checkAgent($member['id']);
 if($operation=='index'){
 	//会员中心
-	if ($_GPC['inviter'] && $member['createtime'] < 5) {
+	if ($_GPC['inviter'] && $member['createtime']+5 < time()) {
 		m('member')->checkFirstInviter($openid, $_GPC['inviter']);
 	}
     if (empty($member['phone'])) {
