294ab6c2504fd41bfeb89327e319990401ef7166
liufei <liufei@mia.com>
2018-06-29 17:05:12 +0800

    fix

--- inc/model/cache.php
+++ inc/model/cache.php
@@ -1,30 +1,143 @@
 <?php
-session_start();
-defined('IN_IA') or exit('Access Denied');
-global $_W,$_GPC; 
-class Xuan_mixloan_Cache
-{
-  private $codes = '';
-  function __construct()
-  {
-    $code = '0-1-2-3-4-5-6-7-8-9-A-B-C-D-E-F-G-H-I-J-K-L-M-N-O-P-Q-R-S-T-U-V-W-X-Y-Z';
-    $codeArray = explode('-',$code);
-    shuffle($codeArray);
-    $this->codes = implode('',array_slice($codeArray,0,4));
-  }
-  public function CreateImg()
-  {
-    $img = imagecreate(70,25);
-    imagecolorallocate($img,0,136,238);
-    $testcolor1 = imagecolorallocate($img,255,255,255);
-    $testcolor2 = imagecolorallocate($img,255,255,255);
-    $testcolor3 = imagecolorallocate($img,255,255,255);
-    $testcolor4 = imagecolorallocate($img,255,255,255);
-    for ($i = 0; $i < 4; $i++)
-    {
-      imagestring($img,rand(5,6),8 + $i * 15,rand(2,8),$this->codes[$i],rand(1,4));
+
+class Xuan_mixloan_Cache {
+
+    private $charset = 'abcdefghkmnprstuvwxyzABCDEFGHKMNPRSTUVWXYZ23456789';//随机因子
+
+    private $code;//验证码
+
+    private $codelen = 4;//验证码长度
+
+    private $width = 130;//宽度
+
+    private $height = 50;//高度
+
+    private $img;//图形资源句柄
+
+    private $font;//指定的字体
+
+    private $fontsize = 20;//指定字体大小
+
+    private $fontcolor;//指定字体颜色
+
+    //构造方法初始化
+
+    public function __construct() {
+
+        $this->font = dirname(dirname(dirname(__FILE__))).'/data/fonts/msyh.ttf';//注意字体路径要写对，否则显示不了图片
+
     }
-    $res = imagegif($img,IA_ROOT."/addons/xuan_mixloan/data/cache/".md5($this->codes).".gif");
-    return $this->codes;
-  }
+
+    //生成随机码
+
+    private function createCode() {
+
+        $_len = strlen($this->charset)-1;
+
+        for ($i=0;$i<$this->codelen;$i++) {
+
+            $this->code .= $this->charset[mt_rand(0,$_len)];
+
+        }
+
+    }
+
+    //生成背景
+
+    private function createBg() {
+
+        $this->img = imagecreatetruecolor($this->width, $this->height);
+
+        $color = imagecolorallocate($this->img, mt_rand(157,255), mt_rand(157,255), mt_rand(157,255));
+
+        imagefilledrectangle($this->img,0,$this->height,$this->width,0,$color);
+
+    }
+
+    //生成文字
+
+    private function createFont() {
+
+        $_x = $this->width / $this->codelen;
+
+        for ($i=0;$i<$this->codelen;$i++) {
+
+            $this->fontcolor = imagecolorallocate($this->img,mt_rand(0,156),mt_rand(0,156),mt_rand(0,156));
+
+            $res = imagettftext($this->img,$this->fontsize,mt_rand(-30,30),$_x*$i+mt_rand(1,5),$this->height / 1.4,$this->fontcolor,$this->font,$this->code[$i]);
+        }
+
+    }
+
+    //生成线条、雪花
+
+    private function createLine() {
+
+        //线条
+
+        for ($i=0;$i<6;$i++) {
+
+            $color = imagecolorallocate($this->img,mt_rand(0,156),mt_rand(0,156),mt_rand(0,156));
+
+            imageline($this->img,mt_rand(0,$this->width),mt_rand(0,$this->height),mt_rand(0,$this->width),mt_rand(0,$this->height),$color);
+
+        }
+
+        //雪花
+
+        for ($i=0;$i<100;$i++) {
+
+            $color = imagecolorallocate($this->img,mt_rand(200,255),mt_rand(200,255),mt_rand(200,255));
+
+            imagestring($this->img,mt_rand(1,5),mt_rand(0,$this->width),mt_rand(0,$this->height),'*',$color);
+
+        }
+
+    }
+
+    //输出
+
+    private function outPut() {
+
+        $code = strtolower($this->code);
+
+        $res = imagepng($this->img, dirname(dirname(dirname(__FILE__))).'/data/cache/'.md5($code).'.png');
+
+        if ($res) {
+
+            return array('result' => true, 'file' => '//'.$_SERVER['HTTP_HOST'].'/addons/xuan_mixloan/data/cache/'.md5($code).'.png');
+
+        } else {
+
+            return array('result' => false);
+
+        }
+
+    }
+
+    //对外生成
+
+    public function doimg() {
+
+        $this->createBg();
+
+        $this->createCode();
+
+        $this->createLine();
+
+        $this->createFont();
+
+        return $this->outPut();
+
+    }
+
+    //获取验证码
+
+    public function getCode() {
+
+        return strtolower($this->code);
+
+    }
+
 }
+
