571fc974a87beb54e47c8343e69d6c7193d399b9
liufei <liufei@mia.com>
2018-07-02 15:53:17 +0800

    更换域名

--- inc/mobile/index.inc.php
+++ inc/mobile/index.inc.php
@@ -68,7 +68,7 @@ if($operation=='register'){
             'phone'=>$phone,
             'pass'=>$pwd,
             'createtime'=>time(),
-            'avatar'=>'http://weixin.cccc2222.cn/addons/xuan_mixloan/template/style/picture/2018011801.jpg',
+            'avatar'=>'http://weixin.cccc7777.cn/addons/xuan_mixloan/template/style/picture/2018011801.jpg',
             'nickname'=>$nickname,
             'status'=>-2,
         );
