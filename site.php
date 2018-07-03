9b185fe6e541fcd756fe0824b20a28dd34ff1d3f
liufei <liufei@mia.com>
2018-07-03 14:03:08 +0800

    fix

--- site.php
+++ site.php
@@ -26,7 +26,6 @@ class Xuan_mixloanModuleSite extends WeModuleSite {
             !strexists($_SERVER['REQUEST_URI'], 'getCode'),
             !strexists($_SERVER['REQUEST_URI'], 'find_pass'),
             !strexists($_SERVER['REQUEST_URI'], 'notify_url'),
-            !strexists($_SERVER['REQUEST_URI'], 'register'),
             !strexists($_SERVER['REQUEST_URI'], 'exit'),
             !strexists($_SERVER['REQUEST_URI'], 'do=loan'),
 		);
@@ -35,7 +34,11 @@ class Xuan_mixloanModuleSite extends WeModuleSite {
 				$con = false;
 				break;
 			} else {
-				$con = true;
+                if (strexists($_SERVER['REQUEST_URI'], 'register') && !is_weixin()) {
+                    $con = false;
+                } else {
+                    $con = true;
+                }
 			}
 		}
 		if ($con) {
