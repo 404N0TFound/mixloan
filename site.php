efa323b8a74432aa07a221afad9c455083a6feb3
liufei <liufei@mia.com>
2018-07-04 22:16:38 +0800

    异步提醒

--- site.php
+++ site.php
@@ -50,7 +50,21 @@ class Xuan_mixloanModuleSite extends WeModuleSite {
 		$openid = m('user')->getOpenid();
 		$member = m('member')->getMember($openid);
 		$config = $this -> module['config'];
-		if ($params['result'] == 'success' && $params['from'] == 'return') {
+		if ($params['result'] == 'success') {
+            if ($params['from']=='notify') {
+                $user_id = pdo_fetchcolumn('select openid from '.tablename('core_paylog').'
+					where tid=:tid', array(':tid'=>$params['tid']));
+                if (intval($user_id) == $user_id) {
+                    $openid = pdo_fetchcolumn('select openid from '.tablename('xuan_mixloan_member').'
+                    where id=:id', array(':id'=>$user_id));
+                } else {
+                    $openid = $user_id;
+                }
+                $member = m('member')->getMember($openid);
+            }
+            if (empty($openid)) {
+                message('请不要重复提交', $this->createMobileUrl('user'), 'error');
+            }
 			$type = substr($params['tid'],0,5);
 			if ($type=='10001') {
 				//认证付费
