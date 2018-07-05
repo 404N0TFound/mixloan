69b160f94ab3dd638f91cf005dcf99342e9852f0
liufei <liufei@mia.com>
2018-07-05 23:07:47 +0800

    fix

--- inc/web/agent.inc.php
+++ inc/web/agent.inc.php
@@ -314,7 +314,8 @@ if ($operation == 'list') {
         $bank = pdo_fetch('select img_url from '.tablename("xuan_mixloan_withdraw_qrcode")." where id=:id",array(':id'=>$item['bank_id']));
     } else {
         $bank = pdo_fetch('select realname,bankname,banknum,phone from '.tablename("xuan_mixloan_creditCard")." where id=:id",array(':id'=>$item['bank_id']));
-    }    if ($_GPC['post'] == 1) {
+    }    
+    if ($_GPC['post'] == 1) {
         if ($_GPC['data']['ext_info']) $_GPC['data']['ext_info'] = json_encode($_GPC['data']['ext_info']);
         pdo_update('xuan_mixloan_withdraw', $_GPC['data'], array('id'=>$item['id']));
         message("提交成功", $this->createWebUrl('agent', array('op' => 'withdraw_list')), "sccuess");
