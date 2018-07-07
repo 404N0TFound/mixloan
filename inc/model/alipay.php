bd567ec8169ed58b185b1a00e2ec7e47ff856273
liufei <liufei@mia.com>
2018-07-07 12:47:07 +0800

    fix

--- inc/model/alipay.php
+++ inc/model/alipay.php
@@ -18,7 +18,7 @@ class Xuan_mixloan_Alipay
      */
     public function transfer($out_biz_no, $amount, $payee_account, $payee_real_name)
     {
-        $payer_show_name        = '用户提现';
+        $payer_show_name        = '洁美';
         $remark                 = '用户提现';
         $aop                    = new \AopClient();
         $aop->gatewayUrl        = $this->gatewayUrl;
