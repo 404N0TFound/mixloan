49ce69104b4af768db4e965c24839db241be0e65
liufei <liufei@mia.com>
2018-07-17 21:38:06 +0800

    fix

--- inc/mobile/index.inc.php
+++ inc/mobile/index.inc.php
@@ -38,7 +38,7 @@ if($operation=='register'){
             'phone'=>$phone,
             'pass'=>$pwd,
             'createtime'=>time(),
-            'avatar'=>'http://juxinwangluo.xin/addons/xuan_mixloan/template/style/picture/2018011801.jpg',
+            'avatar'=>'http://hfpld.com/addons/xuan_mixloan/template/style/picture/2018011801.jpg',
             'nickname'=>$nickname,
             'status'=>-2,
         );
