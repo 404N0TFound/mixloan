9bbda50f1d8938b361496fa2cf0268d71a7f52d6
liufei <liufei@mia.com>
2018-07-17 23:51:24 +0800

    fix

--- inc/web/member.inc.php
+++ inc/web/member.inc.php
@@ -182,6 +182,31 @@ if ($operation == 'list') {
         message("发送成功，总计发送{$count}条，已转入消息发送队列", "", "success");
         
     }
+} else if ($operation == 'msg') {
+    //群发消息
+    if ($_GPC['post']) {
+        $insert = array();
+        $members = pdo_fetchall('select id from ' .tablename('xuan_mixloan_member'). ' where uniacid=:uniacid', array(':uniacid' => $_W['uniacid']));
+        foreach ($members as $member) {
+            $ext_info = array('content' => trim($_GPC['content']), 'remark' => trim($_GPC['remark']), 'url' => trim($_GPC['url']));
+            $temp = array(
+                'is_read'=>0,
+                'uid'=>0,
+                'createtime'=>time(),
+                'uniacid'=>$_W['uniacid'],
+                'to_uid'=>$member['id'],
+                'ext_info'=>"'" . addslashes(json_encode($ext_info)) . "'",
+            );
+            $temp_string = '('. implode(',', array_values($temp)) . ')';
+            $insert[] = $temp_string;
+        }
+        if (!empty($insert)) {
+            $insert_string =  implode(',', $insert);
+            pdo_run("INSERT " .tablename("xuan_mixloan_msg"). " ( `is_read`, `uid`, `createtime`, `uniacid`, `to_uid`, `ext_info`) VALUES {$insert_string}");
+            $count = count($insert);
+            message("发送成功，总计发送{$count}条", "", "success");
+        }
+    }
 }
 include $this->template('member');
 ?>
