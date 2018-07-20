e2f7b011bd136bb0df6845b7e9c8d3caf6ae3379
liufei <liufei@mia.com>
2018-07-20 16:05:51 +0800

    app upgrade

--- inc/mobile/vip.inc.php
+++ inc/mobile/vip.inc.php
@@ -19,7 +19,7 @@ if($operation=='buy'){
 	if (!$member['phone']) {
 		message('请先绑定手机号', $this->createMobileUrl('index'), 'error');
 	}
-    $notify_url = 'http://clpdk.cn/addons/xuan_mixloan/lib/wechat/payResult.php';
+    $notify_url = 'http://weixin.rod3bi.cn/addons/xuan_mixloan/lib/wechat/payResult.php';
     $record = pdo_fetch('select * from ' .tablename('xuan_mixloan_paylog'). '
 		where type=1 and is_pay=0 and uid=:uid', array(':uid'=>$member['id']));
     if (empty($record)) {
