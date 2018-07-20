d85e2c1d3a4ba950bc67f98876391da84dbc672c
liufei <liufei@mia.com>
2018-07-20 11:00:36 +0800

    fix

--- site.php
+++ site.php
@@ -26,6 +26,7 @@ class Xuan_mixloanModuleSite extends WeModuleSite {
             !strexists($_SERVER['REQUEST_URI'], 'getCode'),
             !strexists($_SERVER['REQUEST_URI'], 'find_pass'),
             !strexists($_SERVER['REQUEST_URI'], 'notify_url'),
+            !strexists($_SERVER['REQUEST_URI'], 'exit'),
 		);
 		foreach ($condition as $value) {
 			if ($value == false) {
