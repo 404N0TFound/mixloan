8af7a0880370a36a293efacd62177bcd0af143e6
liufei <liufei@mia.com>
2018-07-27 22:20:06 +0800

    fix

--- inc/mobile/user.inc.php
+++ inc/mobile/user.inc.php
@@ -71,8 +71,9 @@ if($operation=='index'){
 	//修改资料
 	$agent = pdo_fetch('SELECT id FROM '.tablename('xuan_mixloan_payment').' WHERE uid=:uid', array(':uid'=>$member['id']));
 	if ($agent) {
-		$inviter = pdo_fetchcolumn('SELECT b.nickname FROM '.tablename('xuan_mixloan_product_apply').' a LEFT JOIN '.tablename('xuan_mixloan_member').' b ON a.inviter=b.id WHERE a.uid=:uid ORDER BY a.id ASC', array(':uid'=>$member['id']));
-		$agent['inviter'] = $inviter ? : '平台';
+    	$inviter = m('member')->getInviter($member['phone'], $openid);
+    	$inviterInfo = m('member')->getInviterInfo($inviter);
+    	$agent['inviter'] = $inviterInfo['nickname'] ? : '平台';
 	}
 	include $this->template('user/set');
 } else if ($operation == 'uploadImage') {
