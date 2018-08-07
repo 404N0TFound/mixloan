6f91fcfbf63c91e928087e1cb0f237fdd126939f
liufei <liufei@mia.com>
2018-08-07 11:04:09 +0800

    fix

--- inc/mobile/vip.inc.php
+++ inc/mobile/vip.inc.php
@@ -413,8 +413,8 @@ if($operation=='buy'){
 	if ($bonus > $use) {
 		show_json(-1, null, "可提现余额不足");
 	}
-	if ($bonus < 20) {
-		show_json(-1, null, "提现必须大于20");
+	if ($bonus < 10) {
+		show_json(-1, null, "提现必须大于10");
 	}
 	$insert = array(
 		'uniacid'=>$_W['uniacid'],
