6befddd4b1169abfb7bed931a1f5b480db7dff64
liufei <liufei@mia.com>
2018-08-08 17:14:38 +0800

    fix

--- inc/functions.php
+++ inc/functions.php
@@ -180,16 +180,12 @@ function shortUrl($target) {
     $target_url = urlencode($target);
     $short = pdo_fetch("SELECT short_url,createtime FROM ".tablename("xuan_mixloan_shorturl")." WHERE target_url=:target_url ORDER BY id DESC", array(':target_url'=>$target));
     if (!$short || $short['createtime'] < time()-86400) {
-        $long_url = urlencode($target);
-        $url      = "http://suo.im/api.php?format=json&url=".$long_url;
-        $json     = file_get_contents( $url );
-        $arr      = json_decode($json, true);
-        if ($arr['err'] == 0) {
-            pdo_insert('xuan_mixloan_shorturl', ['target_url'=>$target, 'short_url'=>$arr['url'], 'createtime'=>time()]);
-            return $arr['url'];
-        } else {
-            return false;
-        }
+        $url = "http://api.uee.me/api.php?url={$target_url}";
+        $result = trim(file_get_contents($url));
+        $result = explode('://', $result);
+        $result = 'http://' . $result[1];
+        pdo_insert('xuan_mixloan_shorturl', ['target_url'=>$target, 'short_url'=>$result, 'createtime'=>time()]);
+        return $result;
     } else {
         return $short['short_url'];
     }
