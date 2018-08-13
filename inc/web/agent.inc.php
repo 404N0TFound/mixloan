befc989786d5c195e2f21624541bc1a35cba67c6
liufei <liufei@mia.com>
2018-08-13 14:12:39 +0800

    fix

--- inc/web/agent.inc.php
+++ inc/web/agent.inc.php
@@ -315,7 +315,7 @@ if ($operation == 'list') {
     $item = pdo_fetch('select * from '.tablename("xuan_mixloan_withdraw"). " where id={$id}");
     $item['ext_info'] = json_decode($item['ext_info'], true);
     $member = pdo_fetch('select avatar,nickname from '.tablename("xuan_mixloan_member")." where id=:id",array(':id'=>$item['uid']));
-    if ($id) {
+    if ($id<4513) {
         //id 28之后改为微信二维码收款
         $bank = pdo_fetch('select img_url from '.tablename("xuan_mixloan_withdraw_qrcode")." where id=:id",array(':id'=>$item['bank_id']));
     } else {
