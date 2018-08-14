6fa6b3bc8cad1712a08ccafa13b8e4f7886fe982
liufei <liufei@mia.com>
2018-08-14 10:23:03 +0800

    会员冻结

--- inc/web/member.inc.php
+++ inc/web/member.inc.php
@@ -10,7 +10,8 @@ if (empty($_GPC['op'])) {
 if ($operation == 'list') {
     $pindex = max(1, intval($_GPC['page']));
     $psize = 20;
-    $wheres = ' AND status<>-1';
+    $status = $_GPC['status'] != '' ? $_GPC['status'] : 1;
+    $wheres = ' AND status=' . $status;
     if (!empty($_GPC['id'])) {
         $wheres.= " AND id='{$_GPC['id']}'";
     }
@@ -35,12 +36,28 @@ if ($operation == 'list') {
     $total = pdo_fetchcolumn( 'select count(1) from ' . tablename('xuan_mixloan_member') . "where uniacid={$_W['uniacid']} "  . $wheres . ' ORDER BY ID DESC' );
     $pager = pagination($total, $pindex, $psize);
 } else if ($operation == 'delete') {
+    // $member = m('member')->getMember($_GPC['id']);
+    // pdo_update('xuan_mixloan_member', array("status" => -1, 'openid'=>'', 'uid'=>0, 'phone'=>'', 'certno'=>''), array('id'=>$_GPC['id']));
+    // pdo_delete('xuan_mixloan_inviter', array("phone" => $member["phone"]));
+    // pdo_delete('xuan_mixloan_inviter', array("uid" => $_GPC["id"]));
+    // pdo_delete('qrcode_stat', array("qrcid" => $_GPC["id"]));
+    // pdo_delete('qrcode_stat', array("openid" => $member['openid']));
+    // pdo_delete('xuan_mixloan_payment', array("uid" => $_GPC["id"]));
+    pdo_update('xuan_mixloan_member', array('status' => 0), array('id' => $_GPC['id']));
+    message("冻结成功", referer(), 'sccuess');
+} else if ($operation == 'recovery') {
+    // 解冻
+    pdo_update('xuan_mixloan_member', array('status' => 1), array('id' => $_GPC['id']));
+    message("解冻成功", referer(), 'sccuess');
+} else if ($operation == 'true_delete') {
+    // 永久删除
     $member = m('member')->getMember($_GPC['id']);
     pdo_update('xuan_mixloan_member', array("status" => -1, 'openid'=>'', 'uid'=>0, 'phone'=>'', 'certno'=>''), array('id'=>$_GPC['id']));
     pdo_delete('xuan_mixloan_inviter', array("phone" => $member["phone"]));
     pdo_delete('xuan_mixloan_inviter', array("uid" => $_GPC["id"]));
+    pdo_delete('qrcode_stat', array("qrcid" => $_GPC["id"]));
+    pdo_delete('qrcode_stat', array("openid" => $member['openid']));
     pdo_delete('xuan_mixloan_payment', array("uid" => $_GPC["id"]));
-    message("删除成功");
 } else if ($operation == 'agent') {
     //设为代理
     $res = m('member')->checkAgent($_GPC['id']);
