dccc8aef2eee2a26ccf9b835501f7fd141e72403
liufei <liufei@mia.com>
2018-08-18 14:37:34 +0800

    fix

--- inc/mobile/loan.inc.php
+++ inc/mobile/loan.inc.php
@@ -89,7 +89,7 @@ if($operation=='index'){
     $item = m('loan')->getList(['*'], ['id'=>$id])[$id];
     $info = m('product')->getList(['id','is_show'], ['id'=>$pid])[$pid];
     if (empty($info['is_show'])){
-        header("location:{php echo $this->createMobileUrl('product', array('op' => 'allProduct', 'inviter' => $_GPC['inviter']))");
+        header("location:{php echo $this->createMobileUrl('product', array('op' => 'allProduct', 'inviter' => $inviter))}");
         exit();
     }
     include $this->template('loan/apply');
