649ab7b406eea99ef1546d39d6ef2fb74296a124
liufei <liufei@mia.com>
2018-08-18 14:27:28 +0800

    fix

--- inc/mobile/loan.inc.php
+++ inc/mobile/loan.inc.php
@@ -89,7 +89,8 @@ if($operation=='index'){
     $item = m('loan')->getList(['*'], ['id'=>$id])[$id];
     $info = m('product')->getList(['id','is_show'], ['id'=>$pid])[$pid];
     if (empty($info['is_show'])){
-        message('该产品已被下架');
+        header("location:{php echo $this->createMobileUrl('product', array('op' => 'allProduct', 'inviter' => $_GPC['inviter']))");
+        exit();
     }
     include $this->template('loan/apply');
 } else if ($operation == 'apply_submit') {
