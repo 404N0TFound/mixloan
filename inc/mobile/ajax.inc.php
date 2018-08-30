9f26f45522a6f221b755577b2ff1b9c445a959db
liufei <liufei@mia.com>
2018-08-30 10:49:34 +0800

    fix

--- inc/mobile/ajax.inc.php
+++ inc/mobile/ajax.inc.php
@@ -11,6 +11,9 @@ if($operation == 'getCode'){
 	$time = time()-86400;
 	$cache =  rand(111111,999999);
 	$content = "尊敬的用户，您的本次操作验证码为：{$cache}";
+    if (sha1(md5(strtolower($_GPC['img_cache']))) != $_COOKIE['authcode']) {
+        show_json(-1, [], "图形验证码不正确");
+    }
     if ($_GPC['activity'] == 1) {
         $verify = pdo_fetchcolumn("SELECT count(1) FROM ".tablename('xuan_mixloan_member').' WHERE phone=:phone and uniacid=:uniacid', array('phone'=>$phone, ':uniacid'=>$_W['uniacid']));
         if ($verify) {
