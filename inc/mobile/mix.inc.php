310893d1bdf09ce40e3b43b419df5b874170cee3
liufei <liufei@mia.com>
2018-08-31 10:38:47 +0800

    fix

--- inc/mobile/mix.inc.php
+++ inc/mobile/mix.inc.php
@@ -11,5 +11,23 @@ if($operation=='service'){
 } else if ($operation == 'tutorials') {
 	//新手指南
 	include $this->template('mix/tutorials');
+} else if ($operation == 'announce') {
+	//公告
+	$cid = trim($_GPC['cid']);
+	$announce = pdo_fetch('select id,ext_info from ' . tablename('xuan_mixloan_announce') . '
+		where uniacid=:uniacid order by id desc', array(':uniacid' => $_W['uniacid']));
+	if ($announce) {
+		$announce['ext_info'] = json_decode($announce['ext_info'], 1);
+		$record = pdo_fetchcolumn('select count(*) from ' . tablename('xuan_mixloan_announce_record') . '
+			where relate_id=:relate_id and cid=:cid', array(':relate_id'=>$announce['id'], ':cid'=>$cid));
+		if (!$record) {
+			$insert = array();
+			$insert['cid'] = $cid;
+			$insert['relate_id'] = $announce['id'];
+			pdo_insert('xuan_mixloan_announce_record', $insert);
+			show_json(1, [], $announce['ext_info']['content']);
+		}
+	} else {
+		show_json(-1);
+	}
 }
-?>
