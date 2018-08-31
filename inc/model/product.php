d1ec908757fcd7fc7f3cd32f6694136f9ee4dd16
liufei <liufei@mia.com>
2018-08-31 16:22:20 +0800

    fix

--- inc/model/product.php
+++ inc/model/product.php
@@ -327,20 +327,20 @@ class Xuan_mixloan_Product
     }
 
     /**
-    *   邀请列表 
-    **/
+     *   邀请列表
+     **/
     public function getInviteList($params=[]) {
         global $_W;
         $inviter = (int)$params['inviter'];
-        $begin = strtotime($params['begin']);
-        $end = strtotime($params['begin']." +1 month -1 day");
-        $fields = "b.nickname,b.id as uid,a.openid,a.createtime,c.id,d.re_bonus";
+        // $begin = strtotime($params['begin']);
+        // $end = strtotime($params['begin']." +1 month -1 day");
+        $fields = "b.nickname,b.id as uid,a.openid,a.createtime,b.phone";
         // $sql = "SELECT {$fields} FROM ".tablename("qrcode_stat")." a LEFT JOIN ".tablename("xuan_mixloan_member")." b ON a.openid=b.openid LEFT JOIN ".tablename("xuan_mixloan_payment")." c ON b.id=c.uid LEFT JOIN ".tablename("xuan_mixloan_product_apply")." d ON b.id=d.uid WHERE a.qrcid=:qrcid AND a.type=1 AND a.uniacid={$_W['uniacid']} AND a.createtime>={$begin} AND a.createtime<{$end} ORDER BY a.id DESC";
         //取消时间限制
-        $sql = "SELECT {$fields} FROM ".tablename("qrcode_stat")." a LEFT JOIN ".tablename("xuan_mixloan_member")." b ON a.openid=b.openid LEFT JOIN ".tablename("xuan_mixloan_payment")." c ON b.id=c.uid LEFT JOIN ".tablename("xuan_mixloan_product_apply")." d ON b.id=d.uid WHERE a.qrcid=:qrcid AND a.type=1 AND a.uniacid={$_W['uniacid']} GROUP BY a.openid  ORDER BY a.id DESC";
+        $sql = "SELECT {$fields} FROM ".tablename("qrcode_stat")." a LEFT JOIN ".tablename("xuan_mixloan_member")." b ON a.openid=b.openid WHERE a.qrcid=:qrcid AND a.type=1 AND a.uniacid={$_W['uniacid']} ORDER BY a.id DESC";
         $list = pdo_fetchall($sql,array(":qrcid"=>$inviter));
         foreach ($list as $value) {
-            if (!empty($value['uid'])) {
+            if ($value['uid']) {
                 $uids[] = $value['uid'];
             }
         }
@@ -349,8 +349,7 @@ class Xuan_mixloan_Product
             $uids_string = '(' . implode(',', $uids) . ')';
             $con .= " AND a.uid NOT IN {$uids_string}";
         }
-        // $new = pdo_fetchall("SELECT b.nickname,b.openid,a.createtime,a.id,a.re_bonus FROM ".tablename('xuan_mixloan_product_apply').' a LEFT JOIN '.tablename('xuan_mixloan_member')." b ON a.uid=b.id WHERE a.createtime>={$begin} AND a.createtime<{$end} AND a.inviter={$inviter} AND a.pid=0 {$con} ORDER BY a.id DESC ");
-        $new = pdo_fetchall("SELECT b.nickname,b.openid,a.createtime,a.id,a.re_bonus FROM ".tablename('xuan_mixloan_product_apply').' a LEFT JOIN '.tablename('xuan_mixloan_member')." b ON a.uid=b.id WHERE a.inviter={$inviter} AND a.pid=0 {$con} GROUP BY a.uid ORDER BY a.id DESC ");
+        $new = pdo_fetchall("SELECT b.nickname,b.openid,a.createtime,a.id,a.re_bonus FROM ".tablename('xuan_mixloan_product_apply').' a LEFT JOIN '.tablename('xuan_mixloan_member')." b ON a.uid=b.id WHERE a.inviter={$inviter} AND a.pid=0 {$con} ORDER BY a.id DESC ");
         if ($list && $new){
             $list = array_merge($list, $new);
         } else if ($new) {
@@ -368,10 +367,18 @@ class Xuan_mixloan_Product
                 if ($row['id']) {
                     $row['pay'] = 1;
                 } else {
-                    $row['pay'] = 0;
+                    $pay = pdo_fetchcolumn("SELECT count(1) FROM ".tablename('xuan_mixloan_payment').' WHERE uid=:uid', array(':uid'=>$row['uid']));
+                    if ($pay) {
+                        $row['pay'] = 1;
+                    } else {
+                        $row['pay'] = 0;
+                    }
                 }
                 if (!$row['re_bonus']) {
-                    $row['re_bonus'] = 0;
+                    $row['re_bonus'] = pdo_fetchcolumn("SELECT re_bonus FROM ".tablename('xuan_mixloan_product_apply').' WHERE uid=:uid AND pid=0', array(':uid'=>$row['uid']));
+                    if (!$row['re_bonus']) {
+                        $row['re_bonus'] = 0;
+                    }
                 }
                 $row['createtime'] = date('Y-m-d', $row['createtime']);
             }
