b5bd9344ffa2552954ba0e48702577fd180a0e97
liufei <liufei@mia.com>
2018-08-31 15:53:43 +0800

    ix

--- inc/mobile/mix.inc.php
+++ inc/mobile/mix.inc.php
@@ -90,4 +90,26 @@ if($operation=='service'){
     $list = pdo_fetchall('select extra_bonus,createtime from ' . tablename('xuan_mixloan_product_apply') . '
         where inviter=:inviter and type=5 order by id desc', array(':inviter' => $member['id']));
     include $this->template('mix/bonus_record');
+} else if ($operation == 'announce') {
+    //公告
+    $cid = trim($_GPC['cid']);
+    $announce = pdo_fetch('select id,ext_info from ' . tablename('xuan_mixloan_announce') . '
+		where uniacid=:uniacid order by id desc', array(':uniacid' => $_W['uniacid']));
+    if ($announce) {
+        $announce['ext_info'] = json_decode($announce['ext_info'], 1);
+        if (!$cid) {
+            show_json(1, [], $announce['ext_info']['content']);
+        }
+        $record = pdo_fetchcolumn('select count(*) from ' . tablename('xuan_mixloan_announce_record') . '
+			where relate_id=:relate_id and cid=:cid', array(':relate_id'=>$announce['id'], ':cid'=>$cid));
+        if (!$record) {
+            $insert = array();
+            $insert['cid'] = $cid;
+            $insert['relate_id'] = $announce['id'];
+            pdo_insert('xuan_mixloan_announce_record', $insert);
+            show_json(1, [], $announce['ext_info']['content']);
+        }
+    } else {
+        show_json(-1);
+    }
 }
