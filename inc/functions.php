7b7fa0e3abb349fe81bc867c9147e8d28e22c289
liufei <liufei@mia.com>
2018-09-05 11:28:32 +0800

    fix

--- inc/functions.php
+++ inc/functions.php
@@ -167,7 +167,6 @@ function RGBToHex($rgb){
 *   缩短地址
 **/
 function shortUrl($target) {
-    return $target;
     $target_url = urlencode($target);
     $short = pdo_fetch("SELECT short_url,createtime FROM ".tablename("xuan_mixloan_shorturl")." WHERE target_url=:target_url ORDER BY id DESC", array(':target_url'=>$target));
     if (!$short || $short['createtime'] < time()-86400) {
@@ -244,4 +243,23 @@ function base64_image_content($base64_image_content,$path){
     }else{
         return false;
     }
+}
+// 隐藏部分字符串
+function func_substr_replace($str, $replacement = '*', $start = 4, $length = 3)
+{
+    $len = mb_strlen($str,'utf-8');
+    if ($len > intval($start+$length)) {
+        $str1 = mb_substr($str,0,$start,'utf-8');
+        $str2 = mb_substr($str,intval($start+$length),NULL,'utf-8');
+    } else {
+        $str1 = mb_substr($str,0,1,'utf-8');
+        $str2 = mb_substr($str,$len-1,1,'utf-8');
+        $length = $len - 2;
+    }
+    $new_str = $str1;
+    for ($i = 0; $i < $length; $i++) {
+        $new_str .= $replacement;
+    }
+    $new_str .= $str2;
+    return $new_str;
 }
