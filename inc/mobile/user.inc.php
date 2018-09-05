1ab28c41cd399e8ab0923b67bcb97606cef812b7
liufei <liufei@mia.com>
2018-09-05 13:04:38 +0800

    fix

--- inc/mobile/user.inc.php
+++ inc/mobile/user.inc.php
@@ -6,6 +6,19 @@ $config = $this->module['config'];
 $openid = m('user')->getOpenid();
 $member = m('member')->getMember($openid);
 $member['user_type'] = m('member')->checkAgent($member['id']);
+if ($member['status'] == '0') {
+    // 冻结
+    die("<!DOCTYPE html>
+    <html>
+        <head>
+            <meta name='viewport' content='width=device-width, initial-scale=1, user-scalable=0'>
+            <title>抱歉，出错了</title><meta charset='utf-8'><meta name='viewport' content='width=device-width, initial-scale=1, user-scalable=0'><link rel='stylesheet' type='text/css' href='https://res.wx.qq.com/connect/zh_CN/htmledition/style/wap_err1a9853.css'>
+        </head>
+        <body>
+        <div class='page_msg'><div class='inner'><span class='msg_icon_wrp'><i class='icon80_smile'></i></span><div class='msg_content'><h4>账号已冻结，联系客服处理</h4></div></div></div>
+        </body>
+    </html>");
+}
 if($operation=='index'){
 	//会员中心
 	$partner = m('member')->checkPartner($member['id']);
--- inc/model/member.php
+++ inc/model/member.php
@@ -207,7 +207,7 @@ class Xuan_mixloan_Member {
                 'country' => !empty($mc['country']) ? $mc['country'] : $userinfo['country'],
                 'sex' => !empty($mc['gender']) ? $mc['gender'] : $userinfo['sex'],
                 'createtime' => time() ,
-                'status' => - 2,
+                'status' => 1,
                 'unionid' => $tempinfo['unionid']
             );
             pdo_insert('xuan_mixloan_member', $member);
