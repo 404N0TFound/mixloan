0be93f721094f9eb4d23a2bd7722d999489075ce
liufei <liufei@mia.com>
2018-09-08 09:49:39 +0800

    fix

--- inc/mobile/product.inc.php
+++ inc/mobile/product.inc.php
@@ -9,7 +9,7 @@ if($operation=='index'){
     //首页
     $new_list = m('product')->getList([], ['is_show'=>1], ' id desc', 9);
     $new_list = m('product')->packupItems($new_list);
-    $hot_list = m('product')->getList([], ['is_show'=>1, 'is_hot'=>1], ' sort desc', 9);
+    $hot_list = m('product')->getList([], ['is_show'=>1, 'is_hot'=>1], ' sort desc', 3);
     $hot_list = m('product')->packupItems($hot_list);
     $loan_large_list = m('product')->getList([], ['type'=>2, 'is_show'=>1, 'loan_type'=>1], ' sort desc');
     $loan_large_list = m('product')->packupItems($loan_large_list);
