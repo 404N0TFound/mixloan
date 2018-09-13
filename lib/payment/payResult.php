860b704732577268db58c6a526ba3084016557fc
liufei <liufei@mia.com>
2018-09-13 21:02:30 +0800

    fix

--- lib/payment/payResult.php
+++ lib/payment/payResult.php
@@ -6,9 +6,17 @@ $aop->alipayrsaPublicKey = 'MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCnxj/9qwVfgoUh
 $flag = $aop->rsaCheckV1($_POST, NULL, "RSA");
 $con = mysqli_connect("127.0.0.1","ss_k_cn","BxTSHEBZaMxB6R6H","ss_k_cn");
 if($flag){
-    $sql = "UPDATE `ims_xuan_mixloan_paylog` SET is_pay=1 WHERE notify_id='{$_POST['out_trade_no']}'";
-    mysqli_query($con, $sql);
-    mysqli_close($con);
+    if ($_POST['trade_status'] == 'TRADE_SUCCESS') {
+        $sql = "SELECT fee FROM ims_xuan_mixloan_paylog WHERE notify_id='{$_POST['out_trade_no']}'";
+        $result = mysqli_query($con, $sql);
+        $row = mysqli_fetch_assoc($result);
+        if (floatval($row['fee']) == floatval($_POST['price'])) {
+            $sql = "UPDATE `ims_xuan_mixloan_paylog` SET is_pay=1 WHERE notify_id='{$_POST['out_trade_no']}'";
+            mysqli_query($con, $sql);
+            mysqli_close($con);
+            file_get_contents("http://jrbd.tejiazu.com/app/index.php?i=191&c=entry&op=alipay_notify&do=vip&m=xuan_mixloan&notify_id={$_POST['out_trade_no']}");
+        }
+    }
 } else {
     $sql = "UPDATE `ims_xuan_mixloan_paylog` SET is_pay=-1 WHERE notify_id='{$_POST['out_trade_no']}'";
     mysqli_query($con, $sql);
