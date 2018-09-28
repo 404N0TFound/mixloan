912c336e9609a6822880f60301783bfc084dd9b2
liufei <liufei@mia.com>
2018-09-28 11:21:49 +0800

    fix

--- inc/web/statistics.inc.php
+++ inc/web/statistics.inc.php
@@ -75,7 +75,7 @@ if ($operation == 'list')
     }
 } else if ($operation == 'verify') {
     $password = trim($_GPC['password']);
-    if ($password == '123456') {
+    if ($password == 'hq517023') {
         setcookie('verify', 1,time()+86400);
         header("location:{$this->createWebUrl('statistics')}");
     } else {
