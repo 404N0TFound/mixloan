da8ecd611346715fa2d3862074fa0fb296c5081e
liufei <liufei@mia.com>
2018-10-11 20:43:49 +0800

    fix

--- lib/wechat/payResult.php
+++ lib/wechat/payResult.php
@@ -15,7 +15,7 @@ $stringSignTemp = $buff . 'key=lyet24ceael03aj2vo5wq251nu3lf2kn';//key为证书�
 $sign = strtoupper(md5($stringSignTemp));
 //判断算出的签名和通知信息的签名是否一致
 $json = json_encode($data);
-$con = mysqli_connect("127.0.0.1","sql3dmjg_com","esYeNeHN5sxynM5f","sql3dmjg_com");
+$con = mysqli_connect("127.0.0.1","root","d0a2c71fdb154d67","we7");
 if($sign == $data['sign']){
     if ($data['result_code'] == 'SUCCESS') {
         $sql = "INSERT INTO ims_xuan_mixloan_log (ext_info) VALUES ('{$json}')";
@@ -23,7 +23,7 @@ if($sign == $data['sign']){
         $sql = "UPDATE `ims_xuan_mixloan_paylog` SET is_pay=1 WHERE notify_id='{$data['out_trade_no']}'";
         mysqli_query($con, $sql);
         mysqli_close($con);
-        header("location:http://hqph.bjhantangyuanlin.com/app/index.php?i=1&c=entry&op=notify_url" .
+        header("location:http://wx.wd8818.cn/app/index.php?i=1&c=entry&op=notify_url" .
             "&do=vip&m=xuan_mixloan&notify_id={$data['out_trade_no']}");
     } else {
         $sql = "UPDATE `ims_xuan_mixloan_paylog` SET is_pay=-1 WHERE notify_id='{$data['out_trade_no']}'";
