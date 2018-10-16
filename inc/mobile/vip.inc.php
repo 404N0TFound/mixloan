253f5cb88dc34d4b8c2a8a5ff87f68dd25ddff22
liufei <liufei@mia.com>
2018-10-16 13:48:24 +0800

    fix

--- inc/mobile/vip.inc.php
+++ inc/mobile/vip.inc.php
@@ -693,14 +693,23 @@ if($operation=='buy'){
 	include $this->template('vip/degreeDetail');
 } else if ($operation == 'rank_list') {
 	//排行榜
-    $temp_time = date('Y-m') . '-1';
-    $start_time = strtotime($temp_time);
-    $end_time = strtotime("+1 month {$temp_time}");
-    // $list = pdo_fetchall("SELECT inviter,SUM(re_bonus+done_bonus+extra_bonus) AS bonus FROM ".tablename('xuan_mixloan_bonus')." WHERE relate_id=0 AND createtime>{$start_time} AND createtime<{$end_time} GROUP BY inviter HAVING bonus<>0 ORDER BY bonus DESC LIMIT 15");
-    $list = pdo_fetchall("SELECT uid,SUM(bonus) as count_bonus FROM " .tablename('xuan_mixloan_withdraw'). "
-    	WHERE createtime>{$start_time} AND createtime<{$end_time}
-    	GROUP BY uid HAVING count_bonus<>0
-    	ORDER BY count_bonus DESC LIMIT 15");
+    if ($_GPC['day'] == 1) {
+        $strattime = strtotime(date('Y-m-d') . ' -1 days');
+        $endtime = strtotime(date('Y-m-d'));
+        $list = pdo_fetchall("select inviter as uid,SUM(re_bonus+done_bonus+extra_bonus) as count_bonus from " . tablename('xuan_mixloan_bonus') . "
+             WHERE createtime>{$strattime} AND createtime<{$endtime}
+             GROUP BY uid HAVING count_bonus<>0
+             ORDER BY count_bonus DESC LIMIT 15");
+    } else {
+        $temp_time = date('Y-m') . '-1';
+        $start_time = strtotime($temp_time);
+        $end_time = strtotime("+1 month {$temp_time}");
+        // $list = pdo_fetchall("SELECT inviter,SUM(re_bonus+done_bonus+extra_bonus) AS bonus FROM ".tablename('xuan_mixloan_bonus')." WHERE relate_id=0 AND createtime>{$start_time} AND createtime<{$end_time} GROUP BY inviter HAVING bonus<>0 ORDER BY bonus DESC LIMIT 15");
+        $list = pdo_fetchall("SELECT uid,SUM(bonus) as count_bonus FROM " .tablename('xuan_mixloan_withdraw'). "
+            WHERE createtime>{$start_time} AND createtime<{$end_time}
+            GROUP BY uid HAVING count_bonus<>0
+            ORDER BY count_bonus DESC LIMIT 15");
+    }
     if (!empty($list)) {
         foreach ($list as &$row) {
             $temp_member = pdo_fetch("SELECT nickname,avatar,phone FROM ".tablename('xuan_mixloan_member').' WHERE id=:id', array(':id'=>$row['uid']));
