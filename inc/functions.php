98709a9e9c50524064848ea9759ff22b72f03ce0
liufei <liufei@mia.com>
2018-11-17 14:56:31 +0800

    fix

--- inc/functions.php
+++ inc/functions.php
@@ -174,12 +174,12 @@ function RGBToHex($rgb){
 function shortUrl($target) {
     $target_url = urlencode($target);
     $short = pdo_fetch("SELECT short_url,createtime FROM ".tablename("xuan_mixloan_shorturl")." WHERE target_url=:target_url ORDER BY id DESC", array(':target_url'=>$target));
-    if (!$short || $short['createtime'] < time()-86400) {
+    if (!$short || $short['createtime'] < time()-86400*7) {
         $long_url = urlencode($target);
-        $url      = "http://suo.im/api.php?format=json&url=".$long_url;
+        $url      = "https://12i.cn/api.ashx?format=json&userId=2642&key=9C1064B741249E99A81833BDD89D3C6B&url=".$long_url;
         $json     = file_get_contents( $url );
         $arr      = json_decode($json, true);
-        if ($arr['err'] == 0) {
+        if ($arr) {
             pdo_insert('xuan_mixloan_shorturl', ['target_url'=>$target, 'short_url'=>$arr['url'], 'createtime'=>time()]);
             return $arr['url'];
         } else {
