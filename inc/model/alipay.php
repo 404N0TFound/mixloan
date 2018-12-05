7b5e7e3db74aff5a0fa1866e12f12a59d1c3a056
liufei <liufei@mia.com>
2018-12-05 16:20:26 +0800

    fix

--- inc/model/alipay.php
+++ inc/model/alipay.php
@@ -1,7 +1,7 @@
 <?php
 defined('IN_IA') or exit('Access Denied');
-require_once('/www/wwwroot/xz_zhongchuangkm_com/addons/xuan_mixloan/lib/alipay/aop/AopClient.php');
-require_once('/www/wwwroot/xz_zhongchuangkm_com/addons/xuan_mixloan/lib/alipay/aop/request/AlipayFundTransToaccountTransferRequest.php');
+require_once('/www/wwwroot/xz.zhongchuangkm.com/addons/xuan_mixloan/lib/alipay/aop/AopClient.php');
+require_once('/www/wwwroot/xz.zhongchuangkm.com/addons/xuan_mixloan/lib/alipay/aop/request/AlipayFundTransToaccountTransferRequest.php');
 class Xuan_mixloan_Alipay
 {
     private $gatewayUrl = 'https://openapi.alipay.com/gateway.do';
