37de57aa0cab7e26137c2d76ff8db909b37d214d
liufei <liufei@mia.com>
2018-12-25 16:42:12 +0800

    fix

--- inc/mobile/product.inc.php
+++ inc/mobile/product.inc.php
@@ -106,7 +106,7 @@ if($operation=='index'){
 	if ($id <= 0) {
 		show_json(-1, [], "id为空");
 	}
-	if(!trim($_GPC['name']) || !trim($_GPC['phone'])) {
+	if(!trim($_GPC['phone'])) {
 		show_json(-1, [], '资料不能为空');
 	}
 	$record = m('product')->getApplyList(['id'], ['pid'=>$id, 'phone'=>$_GPC['phone']]);
