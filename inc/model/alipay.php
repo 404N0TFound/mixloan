35be19b39b0ab0f7ad98928948071a7319c5a06e
liufei <liufei@mia.com>
2019-03-01 12:38:56 +0800

    fix

--- inc/model/alipay.php
+++ inc/model/alipay.php
@@ -34,9 +34,7 @@ class Xuan_mixloan_Alipay
         $record = pdo_fetch('select id,createtime from ' . tablename('xuan_mixloan_alipay_log') . ' where 
             account=:account order by id desc', array(':account' => $id));
         if (!empty($record)) {
-            if ($record['createtime'] + 86400 > time()) {
-                return array('code' => -1, 'msg' => '不允许重复打款');
-            }
+            return array('code' => -1, 'msg' => '不允许重复打款');
         } else {
             $insert = array();
             $insert['account'] = $account;
