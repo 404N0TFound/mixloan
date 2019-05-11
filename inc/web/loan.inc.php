9f0d72ca3df50e549a17e3e6c64595f5a7529db4
liufei <liufei@mia.com>
2019-05-11 14:00:08 +0800

    fix

--- inc/web/loan.inc.php
+++ inc/web/loan.inc.php
@@ -26,7 +26,6 @@ if ($operation == 'list') {
     //添加
     if ($_GPC['post'] == 1) {
         $data = $_GPC['data'];
-        $data['apply_nums'] = 500;
         $data['uniacid'] = $_W['uniacid'];
         $data['type'] = implode(',', $data['type']); 
         $data['createtime'] = time();
